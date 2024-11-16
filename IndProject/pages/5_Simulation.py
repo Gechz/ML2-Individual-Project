4978a2ce77d6cd33a99ef5454131aa9cf640c894
Gabriel Chapman <gechz96@gmail.com>
2024-11-16 15:03:19 +0100

    Update 5_Simulation.py

--- IndProject/pages/5_Simulation.py
+++ IndProject/pages/5_Simulation.py
@@ -25,8 +25,8 @@ def run_simulation():
     **Note**: `MedInc` has been converted to a nominal value (not thousands).
     """)
     
-    med_inc = st.number_input('Median Income (nominal)', min_value=500.0, max_value=20000.0, step=1000.0, format="%.1f")
-    population = st.number_input('Population', min_value=50.0, max_value=50000.0, step=500.0, format="%.1f")
+    med_inc = st.number_input('Median Income (nominal) in the Block Group', min_value=500.0, max_value=20000.0, step=1000.0, format="%.1f")
+    population = st.number_input('Population in the Block', min_value=50.0, max_value=50000.0, step=500.0, format="%.1f")
     
     house_age = st.slider('House Age', min_value=1, max_value=80, step=1, value=30)
     ave_rooms = st.slider('Average Number of Rooms', min_value=1, max_value=20, step=1, value=5)
