03f8a8fa8e472e38cefff71863a5ac8575c925d6
Gabriel Chapman <gechz96@gmail.com>
2024-11-16 14:25:16 +0100

    Update 4_ANN Model Result.py

--- IndProject/pages/4_ANN Model Result.py
+++ IndProject/pages/4_ANN Model Result.py
@@ -12,7 +12,7 @@ def ann_model_result():
     This plot shows the training and validation loss over epochs. The training loss curve indicates how well the model is learning on the training data, while the validation loss helps to monitor its generalization on unseen data. 
     A consistent gap between the curves or an increasing validation loss while training loss continues to decrease might indicate overfitting. This furthermore indicates that the model was stopped after roughly 35 epochs to prevent overfitting.
     """)
-    st.image("training_validation_loss.png")  # Replace with the correct path to your image
+    st.image("IndProject/training_validation_loss.png")  # Replace with the correct path to your image
     st.code("""
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -26,7 +26,7 @@ def ann_model_result():
     st.markdown("""
     This scatter plot illustrates the predicted values against the true values from the test set, in their standardized form. A strong positive diagonal pattern indicates good predictive performance.
     """)
-    st.image("standardized_predictions.png")  # Replace with the correct path to your image
+    st.image("IndProject/standardized_predictions.png")  # Replace with the correct path to your image
     st.code("""
     y_predict = model1.predict(X_test)
     plt.plot(y_test, y_predict, "^", color='r')
@@ -39,7 +39,7 @@ def ann_model_result():
     st.markdown("""
     This plot provides a more interpretable view by showing the true values against the predicted values on their original scale. A pattern close to a 45-degree line suggests accurate predictions.
     """)
-    st.image("original_scale_predictions.png")  # Replace with the correct path to your image
+    st.image("IndProject/original_scale_predictions.png")  # Replace with the correct path to your image
     st.code("""
     # Reshape y_predict to have the same number of features as the scaler expects
     y_predict_reshaped = y_predict.reshape(-1, 1)
@@ -59,7 +59,7 @@ def ann_model_result():
     st.markdown("""
     The table below displays the evaluation metrics for the test set, including R quared (R^2), Root Mean Squared Error (RMSE), Mean Squared Error (MSE), and Mean Absolute Error (MAE). These metrics provide insights into the model's prediction accuracy on unseen data.
     """)
-    st.image("test_results.png")  # Replace with the correct path to your image
+    st.image("IndProject/test_results.png")  # Replace with the correct path to your image
     st.code("""
     from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
     from math import sqrt
@@ -87,7 +87,7 @@ def ann_model_result():
     st.markdown("""
     The train set metrics provide an understanding of how well the model performs on the training data. This can help to identify if the model has overfit the data or is generalizing well.
     """)
-    st.image("train_results.png")  # Replace with the correct path to your image
+    st.image("IndProject/train_results.png")  # Replace with the correct path to your image
     st.code("""
     from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
     from math import sqrt
