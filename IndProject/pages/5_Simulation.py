4cf8e41bfb0860b41ecdb3f861950e282f0663f3
Gabriel Chapman <gechz96@gmail.com>
2024-11-16 15:02:40 +0100

    Update 5_Simulation.py

--- IndProject/pages/5_Simulation.py
+++ IndProject/pages/5_Simulation.py
@@ -26,7 +26,7 @@ def run_simulation():
     """)
     
     med_inc = st.number_input('Median Income (nominal)', min_value=500.0, max_value=20000.0, step=1000.0, format="%.1f")
-    population = st.number_input('Population', min_value=5.0, max_value=50000.0, step=500.0, format="%.1f")
+    population = st.number_input('Population', min_value=50.0, max_value=50000.0, step=500.0, format="%.1f")
     
     house_age = st.slider('House Age', min_value=1, max_value=80, step=1, value=30)
     ave_rooms = st.slider('Average Number of Rooms', min_value=1, max_value=20, step=1, value=5)
